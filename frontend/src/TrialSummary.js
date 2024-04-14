f3e0286bcd942fed3a9139495385fd613ca00371
yangjus <justiny_2013@yahoo.com>
2024-04-14 11:09:11 -0400

    day 2

--- frontend/src/TrialSummary.js
+++ frontend/src/TrialSummary.js
@@ -4,16 +4,18 @@ const TrialSummary = ({ summary }) => {
     // summary is a JSON of {Title, Date, Experiment, Results, Limitations}
   return (
     <div className="container">
-        <h2>Title: </h2>
-        <p>{summary.Title}</p>
-        <h2>Date: </h2>
-        <p>{summary.Date}</p>
-        <h2>Experiment: </h2>
-        <p>{summary.Experiment}</p>
-        <h2>Results: </h2>
-        <p>{summary.Results}</p>
-        <h2>Limitations: </h2>
-        <p>{summary.Limitations}</p>
+        <div className="card">
+            <h3 className="card-h3">Title</h3>
+            <p>{summary.Title}</p>
+            <h3>Date</h3>
+            <p>{summary.Date}</p>
+            <h3>Experiment</h3>
+            <p>{summary.Experiment}</p>
+            <h3>Results</h3>
+            <p>{summary.Results}</p>
+            <h3>Limitations</h3>
+            <p>{summary.Limitations}</p>
+        </div>
     </div>
   );
 };
